79d8c11c3e8d53019ce391a1e29074b353eef145
kapadiamush <kapadiamush@gmail.com>
2014-07-27 12:34:10 -0400

    blank updates

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.get('/yo/:username', function(req, res){ //send a yo
 app.post('/register', function(req, res){ 
 	console.log(req.body);
 	var good = register.register(req.body.username, req.body.symbol, req.body.price); 
-	});
 });
 
 /* static routes */
