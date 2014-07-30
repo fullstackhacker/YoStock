a67acf48794c9483fc9996c79ea8f265781d194c
kapadiamush <kapadiamush@gmail.com>
2014-07-30 19:43:04 -0400

    rewrote registeration profile, offically can send yo when get the right price

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var queryString = require('querystring');
+var request = require('request');
 var yo = require('./yo');
-var register = require('./register');
 
 var app = express();
 
 app.use(bodyParser.json());
+
+var users = new Array();
 
 /* routes */
 app.get('/', function(req, res){ //root route
 	res.sendfile("./public/views/register.html");
 });
 
-app.get('/yo/:username', function(req, res){ //send a yo
-	var username = req.params.username	
-	var response = yo.yo(username);
-	res.send(response);
-});
-
 app.post('/register', function(req, res){ 
-	console.log(req.body);
-	var good = register.register(req.body.username, req.body.symbol, req.body.price); 
-	if(good){ 
-		console.log("GOOD AS SHIT!");
-		var data = { 
-			'registration' : 'success'
-		}
-		res.send([{ 'register': 'success' }]);
+	var user = { 
+		name : req.body.username,
+		symbol : req.body.symbol, 
+		price : req.body.price
+	}; 
+	users.push(user);
+	var registration = { 
+		'registration': 'success'
 	}
+	res.json(registration);
 });
 
 /* static routes */
@@ -42,6 +40,28 @@ app.get('/public/stylesheets/reset.css', function(req, res){ // stylesheet route
 app.get('/public/scripts/register.js', function(req, res){
 	res.sendfile("./public/scripts/register.js");
 });
+
+/* App check against yahoo's API every second */
+setInterval(function (){ 
+	for(user in users){
+		console.log(users);
+
+		var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20%3D%20%27"+users[user].symbol+"%27%3B&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+		request.get(url, function(err, response, body){ 
+			var jbody = JSON.parse(body);
+			var lastPrice = Number(jbody.query.results.quote.LastTradePriceOnly);
+			if(users[user]  && lastPrice == users[user].price){ 
+				console.log("FOUND A MATCH-- YO! :: " + users[user].name);
+				yo.yo(users[user].name); //send a yo to that user
+				users.splice(user, 1);
+				return;
+			}
+		});
+		
+		continue;
+	}
+}, 1000);
 
 /* port */
 var port = Number(process.env.PORT || 5000);
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -14,7 +14,6 @@ function register(){
 	var request = new XMLHttpRequest(); 
 	request.open("POST", "/register", true); 
 	request.setRequestHeader("Content-type", "application/json"); 
-	request.send(JSON.stringify(object));
-	
-	console.log(request.responseText);
+	request.send(JSON.stringify(object));	
+	console.log(JSON.parse(request.response));
 } 
