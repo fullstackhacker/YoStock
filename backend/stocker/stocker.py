e7601c2f2bfcc770e858cb19fafe46422227523b
kapadiamush <kapadiamush@gmail.com>
2015-06-17 16:50:15 -0400

    backend yo's when stock falls within price range

--- backend/stocker/stocker.py
+++ backend/stocker/stocker.py
@@ -4,6 +4,7 @@ import sys
 import threading
 import requests
 import time
+import os
 from yahoo_finance import Share
 
 # connect to the rabbitmq server
@@ -24,22 +25,29 @@ def stalk(stock):
     bad = True
     share = Share(stock['symbol'])
 
-    while bad:
-        price = float(share.get_price())
-        print stock['symbol'] + ": " +  str(price) + " +/- " + str(stock['range'])
+    price = float(share.get_price())
 
+    while bad:
         # check if in range
         if(price > stock['price'] - stock['range'] and price < stock['price'] + stock['range']): 
             #send a yo
-            print "yo yo yo " + stock['username']
+            data = { 
+                'username': stock['username'],
+                'api_token': os.environ.get('API_TOKEN')
+            }
             
-            
+            res = requests.post('http://api.justyo.co/yo/', params=data)
+            print res
+
 
             bad = False
         else: 
             share.refresh()
 
-    
+    # send request to delete it
+    _id = stock['_id']
+    deleteUrl = "http://localhost/api/deregister/" + _id
+   # res = requests.delete(deleteUrl)
 
 '''
 callback for the queue
